ed9919b59a3aa6cf8e7ce9060798c45f92468719
Justin Jovic <jjovic@Justins-MacBook-Pro.local>
2019-08-27 20:47:09 -0700

    added code to check of folders already exist.

--- text2folders.py
+++ text2folders.py
@@ -11,7 +11,10 @@ f= open(filename, 'r')
 #loop through each line in a textfile and make a new directory using that name
 for i in f:
     i = i.strip()
-    os.mkdir(i)
-    print ("Created folder", i)
+    if not os.path.exists(i):
+        os.mkdir(i)
+        print ("Created folder", i)
+    else :
+        print("Folder", i, "already exists... moving on.")
 f.close()
 print ("Complete")
