3153640cd78b4e2a98b8cafeec3fd46b071a9b62
Justin Jovic <JustinJo@dpr.com>
2019-08-28 14:41:39 -0700

    added error checking and refactored code

--- text2folders.py
+++ text2folders.py
@@ -4,17 +4,23 @@
 import os
 import sys
 
-#get filename from command line argument
-filename = sys.argv[1]
-f= open(filename, 'r')
+def makefolders(filename):
+    f= open(filename, 'r')
+    #loop through each line in a textfile and make a new directory using that name
+    for i in f:
+        i = i.strip()
+        if not os.path.exists(i):
+            os.mkdir(i)
+            print ("Created folder", i)
+        else :
+            print("Folder", i, "already exists... moving on.")
+    f.close()
+    print ("Complete")
 
-#loop through each line in a textfile and make a new directory using that name
-for i in f:
-    i = i.strip()
-    if not os.path.exists(i):
-        os.mkdir(i)
-        print ("Created folder", i)
-    else :
-        print("Folder", i, "already exists... moving on.")
-f.close()
-print ("Complete")
+#get filename from command line argument
+try:
+    filename = sys.argv[1]
+    makefolders(filename)
+    
+except:
+    print ("You must add the filename of the text file as a command line argument (e.g. python3 text2folders.py <filename.txt>)")
