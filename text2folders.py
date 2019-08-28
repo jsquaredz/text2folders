aa07a4efbdc6ebf5b99dec046ec6a560b8fda1ba
Justin Jovic <jjovic@Justins-MacBook-Pro.local>
2019-08-27 19:12:23 -0700

    update

--- text2folders.py
+++ text2folders.py
@@ -1,34 +1,10 @@
 #!/usr/bin/env python3
-
 #text2files.py
-#
 #usage:   python3 text2folders.py <textfile>
-
 import os
 import sys
 
-#get filename from 
-filename = sys.argv[1]
-f= open(filename, 'r')
-
-#loop through each line in a textfile and make a new directory 
-#using that name
-
-for i in f:
-    i = i.strip()
-    os.mkdir(i)
-    print ("Created folder", i)
-
-f.close()
-print ("Complete")
-#text2files.py
-#
-#usage:   python3 text2folders.py <textfile>
-
-import os
-import sys
-
-#get filename from argument
+#get filename from command line argument
 filename = sys.argv[1]
 f= open(filename, 'r')
 
@@ -37,6 +13,5 @@ for i in f:
     i = i.strip()
     os.mkdir(i)
     print ("Created folder", i)
-
 f.close()
 print ("Complete")
